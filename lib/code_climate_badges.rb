690b7da7dd54f3542bcc0c0d811b2a066ccf75e0
Sam Joseph <tansaku@gmail.com>
2016-07-05 17:11:02 +0100

    shortens method

--- lib/code_climate_badges.rb
+++ lib/code_climate_badges.rb
@@ -13,10 +13,6 @@ class CodeClimateBadges
   def gpa
     response = HTTParty.get(gpa_badge_url)
     stat_regex = /fill-opacity=".3">.*?fill-opacity=".3">([^<]+)/
-    if response.body =~ stat_regex
-        stat = $1 
-    else
-        stat = nil
-    end
+    response.body =~ stat_regex ? $1 : nil
   end
 end
