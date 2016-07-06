aa3578ade76c5d407e93efa89bf86f461824f739
Sam Joseph <tansaku@gmail.com>
2016-07-06 10:18:25 +0100

    adds .gitignore and fixes indenting

--- spec/codeclimate_badges_spec.rb
+++ spec/codeclimate_badges_spec.rb
@@ -1,33 +1,35 @@
 require 'code_climate_badges'
 require 'spec_helper'
 
-describe CodeClimateBadges, vcr: { :allow_unused_http_interactions => false } do
+describe CodeClimateBadges, vcr: {:allow_unused_http_interactions => false} do
 
-	context 'WebsiteOne', :vcr do
-  	subject(:badges) { described_class.new 'github/AgileVentures/WebsiteOne' }
+  context 'WebsiteOne', :vcr do
+    subject(:badges) { described_class.new 'github/AgileVentures/WebsiteOne' }
 
-	  describe '#gpa_badge_url' do 
-	    it { expect(badges.gpa_badge_url).to eq 'https://codeclimate.com/github/AgileVentures/WebsiteOne/badges/gpa.svg' }
-	  end
+    describe '#gpa_badge_url' do
+      it { expect(badges.gpa_badge_url).to eq 'https://codeclimate.com/github/AgileVentures/WebsiteOne/badges/gpa.svg' }
+    end
 
     describe '#gpa', :vcr do
-      it 'grabs gpa from the badge', :vcr do expect(badges.gpa).to eq '3.5' end
+      it 'grabs gpa from the badge', :vcr do
+        expect(badges.gpa).to eq '3.5'
+      end
     end
-	end
-	context 'LocalSupport' do
-		subject(:badges){described_class.new 'github/AgileVentures/LocalSupport'}
-    describe '#gpa_badge_url' do 
+  end
+  context 'LocalSupport' do
+    subject(:badges) { described_class.new 'github/AgileVentures/LocalSupport' }
+    describe '#gpa_badge_url' do
       it { expect(badges.gpa_badge_url).to eq 'https://codeclimate.com/github/AgileVentures/LocalSupport/badges/gpa.svg' }
     end
     describe '#gpa' do
       it { expect(badges.gpa).to eq '3.2' }
     end
-	end  
+  end
 
-	context 'WSO with trailing slash' do
-		subject(:badges) { described_class.new 'github/AgileVentures/WebsiteOne/' }
-	  describe '#gpa_badge_url' do 
-	    it { expect(badges.gpa_badge_url).to eq 'https://codeclimate.com/github/AgileVentures/WebsiteOne/badges/gpa.svg' }
-	  end
-	end
+  context 'WSO with trailing slash' do
+    subject(:badges) { described_class.new 'github/AgileVentures/WebsiteOne/' }
+    describe '#gpa_badge_url' do
+      it { expect(badges.gpa_badge_url).to eq 'https://codeclimate.com/github/AgileVentures/WebsiteOne/badges/gpa.svg' }
+    end
+  end
 end
