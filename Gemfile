88f15dfae8679a7895e8fa3cf6a9cb39f665745e
Sam Joseph <tansaku@gmail.com>
2016-07-19 17:08:07 +0100

    upgrades vcr to get working with latest version of webmock

--- Gemfile
+++ Gemfile
@@ -2,10 +2,13 @@ source 'https://rubygems.org'
 
 gem 'httparty'
 
-group :development do 
+group :test do 
   gem 'rspec', '3.4'
   gem 'vcr'
   gem 'webmock'
+end
+
+group :development, :test do  
   gem 'byebug'
   gem 'sinatra'
 end
--- spec/codeclimate_badges_spec.rb
+++ spec/codeclimate_badges_spec.rb
@@ -1,17 +1,17 @@
 require 'code_climate_badges'
 require 'spec_helper'
 
-describe CodeClimateBadges, vcr: {:allow_unused_http_interactions => false} do
+describe CodeClimateBadges, :vcr do
 
-  context 'WebsiteOne', :vcr do
+  context 'WebsiteOne' do
     subject(:badges) { described_class.new 'github/AgileVentures/WebsiteOne' }
 
     describe '#gpa_badge_url' do
       it { expect(badges.gpa_badge_url).to eq 'https://codeclimate.com/github/AgileVentures/WebsiteOne/badges/gpa.svg' }
     end
 
-    describe '#gpa', :vcr do
-      it 'grabs gpa from the badge', :vcr do
+    describe '#gpa' do
+      it 'grabs gpa from the badge' do
         expect(badges.gpa).to eq '3.5'
       end
     end
